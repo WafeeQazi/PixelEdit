dc328a97909bb45059a63911f91f2134f405674a
Wafee Qazi <wafqaz8@gmail.com>
2026-08-16 23:39:37 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from pixel_edit.main_window import MainWindow
+
+def main() -> None:
+    app = QApplication(sys.argv)
+    app.setApplicationName("PixelEdit")
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
